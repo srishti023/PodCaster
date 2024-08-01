9440d412c82098b116022a1ad46d243551268ca1
srishti <srishtichoudhary52@gmail.com>
2024-08-02 00:45:57 +0530

    sliding navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
     const [MobileNav, setMobileNav] = useState(false);
+    const closeMobileNav = () => setMobileNav(false);
     const navLinks = [
         {
             name:"Home",
@@ -42,26 +43,26 @@ const Navbar = () => {
                 <Link className='px-6 py-3 border-black rounded-full'>Login</Link>
                 <Link className='px-6 py-3 bg-black text-white rounded-full ms-4'>Signup</Link>
         </div>
-        <div className='w-4/6 flex items-center justify-end lg:hidden z-[1000]'>
-            <button className='text-4xl' onClick={()=>setMobileNav(!MobileNav)}>
-            <IoReorderThreeOutline />
+        <div className='w-4/6 flex items-center justify-end lg:hidden z-50'>
+            <button className={`text-4xl ${MobileNav ? "rotate-360" : "rotate-180"} transition-all duration-300`} onClick={()=>setMobileNav(!MobileNav)}>
+            {MobileNav ? <RxCross2/> : <IoReorderThreeOutline />}
             </button>
         </div>
     </div>
-    <div className={`fixed top-0 left-0 w-full h-screen bg-blue-100 ${MobileNav? "translate-y-[0%]" : "translate-y-[-100%]"} transition-all duration-500`}>
-        <div className='p-4 flex items-center justify-end text-3xl z-[2000]'>
-            <button onClick={()=>setMobileNav(!MobileNav)} className='bg-black text-white rounded-full p-2'><RxCross2 /></button>
-        </div>
+
+    {/* MobileNav */}
+    <div className={`fixed top-0 left-0 w-full h-screen bg-blue-100 ${MobileNav? "translate-y-0" : "translate-y-[-100%]"} transition-transform duration-500 ease-in-out`}>
+        
         <div className='h-full flex flex-col items-center justify-center'>
         {navLinks.map((items,i)=>(
-                <Link key={i} to={items.path} className="mb-12 text-2xl hover:font-semibold transition-all duration-300 cursor-pointer">
+                <Link key={i} to={items.path} className="mb-12 text-2xl hover:font-semibold transition-all duration-300" onClick={closeMobileNav}>
                     {items.name}
                 </Link>
             ))}
-            <Link to="/login" className="mb-12 text-2xl hover:font-semibold transition-all duration-300 cursor-pointer">
+            <Link to="/login" className="mb-12 text-2xl hover:font-semibold transition-all duration-300" onClick={closeMobileNav}>
                 Login
             </Link>
-            <Link to="/signup" className="mb-12 text-2xl hover:font-semibold transition-all duration-300 cursor-pointer">
+            <Link to="/signup" className="mb-12 text-2xl hover:font-semibold transition-all duration-300">
                 Signup
             </Link>
         </div>
